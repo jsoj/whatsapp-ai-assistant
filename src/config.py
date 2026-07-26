9520fbf91a9510a98df54de3addccdf9c8f2866c
José Junior <jsoj@github.com>
2026-07-26 17:00:09 -0300

    fix: update default EVOLUTION_URL to evolution.quantisia.com.br

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ env_path = Path(__file__).resolve().parent.parent / ".env"
 load_dotenv(dotenv_path=env_path)
 
 class Settings:
-    EVOLUTION_URL: str = os.getenv("EVOLUTION_URL", "https://evolution.projetobrasil2050.site").rstrip("/")
+    EVOLUTION_URL: str = os.getenv("EVOLUTION_URL", "https://evolution.quantisia.com.br").rstrip("/")
     EVOLUTION_APIKEY: str = os.getenv("EVOLUTION_APIKEY", "6CBB7DCE6D50-4851-A607-F2EC2C1580C2")
     EVOLUTION_INSTANCE: str = os.getenv("EVOLUTION_INSTANCE", "01")
 
